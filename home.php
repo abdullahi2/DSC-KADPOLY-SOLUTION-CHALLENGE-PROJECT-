5386fbfe0451f8e4f45528e603efda44a09eab37
Suleiman Abdullahi <suleimana345@gmail.com>
2021-03-27 08:50:53 +0100

    added home file and images folder

---
+++ home.php
@@ -0,0 +1,52 @@
+<!-- <h1><?php echo $title;?></h1> -->
+
+<div class="col-lg-12">  
+<h4>Mandate</h4>
+    <p> DSC Student E-Learnig primarily provides advance instruction and professional training in all educational sections including science and technology, arts and humanities, education and other related fields. It shall also undertake research, extension services and production activities and provide progressive learning in all areas of specialization.</p> 
+</div>
+<div  class="col-lg-6">
+  <h4>Vision</h4> 
+  <p>Center of Academic Excellence Delivering Quality Learning to all.</p>
+  <h4>Mission</h4>
+  <p> DSC Student E-Learnig for all student is committed to provide advanced knowledge and innovation; develop skills, talents and values; undertake relevant research; development and extension services.</p>
+  <h4>Goals</h4> 
+  <ol>
+    <li>Globally competitive graduates</li>  
+    <li>Institutionalize research culture</li> 
+    <li>Meet the learning style or needs of students.</li> 
+    <li>Enhance the quality of learning and teaching.</li>  
+    <li>Improve user-accessibility and time flexibility to engage learners in the learning process. </li>  
+  </ol>
+
+  <h4>Objectives</h4> 
+   <ol>
+      <li>Setting effective eLearning goals is important in implementing eLearning as it will help in producing great results</li>
+      <li>Provide digitally and through face-to-face learning means for student</li>
+     
+   </ol> 
+</div>
+<div  class="col-lg-6">
+  <h4>Guiding Principles</h4> 
+    <ul>
+      <li>Academic Freedom </li>
+      <li>Responsibility</li>
+      <li> Academic Standards</li>  
+    </ul>
+<h4>Core Values</h4>
+  <ul>
+    <li>Learning Freedom</li>
+    <li>Excellence</li>
+    <li>Integrity</li>
+    <li>Transparency </li>
+    <li>Dedication to Quality Learning</li>  
+  </ul> 
+<h4>Quality Policy</h4> 
+<ol>
+    <li>We are committed to be the center of academic excellence delivering quality learning to all by;</li> 
+    <li>Continuing innovations and quality improvement cultivating an efficient and effective and environment for maximum learning satisfaction;</li> 
+    <li>Adhering to laws and regulations, global learning standards, and educational change requirements;</li> 
+    
+</ol>
+</div>
+ 
+
