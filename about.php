c600ca369df323963c96d04664c71252794b14a4
Suleiman Abdullahi <suleimana345@gmail.com>
2021-03-27 08:21:53 +0100

    added project files

---
+++ about.php
@@ -0,0 +1,8 @@
+<h1><?php echo $title;?></h1>
+<p>Welcome to Developer Student Clubs, Kaduna Polytechnic. We're pleased to have you join our community. For new members, kindly go to our page and register to join our community on WhatsApp and follow us on Facebook and twitter handles. We can't wait to see you there!</p>
+
+<p>Developer Student Clubs are university based community groups for students interested in Google developer technologies. Students from all undergraduate or graduate programs with an interest in growing as a developer are welcome. By joining a DSC, students grow their knowledge in a peer-to-peer learning environment and build solutions for local businesses and their community..</p>
+
+<p>The hub accommodates students who are ready to learn and curious about technology to solve problems in their community.  good set-ups, and a conducive environment are being provided for students here, at the hub Just be passion to learn and come with your system is FREE</p>
+
+<p>We prioritize every student neglecting the race, background, colour, accent/language, or physical challenges of each student. With us, every student is important and unique! Feel free to join any of our sessions and train yourself to become a better problem solver!</p>
